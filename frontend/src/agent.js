1c1fff61be9c02aa2e72b6ca372b1a971b14acbb
michael ellerbeck <michael.ellerbeck@terex.com>
2022-08-19 12:26:19 -0700

    prod

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const API_ROOT =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://api.anythink.market/api";
+    : "https://anythink-market-3yft0-api.herokuapp.com/api";
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
